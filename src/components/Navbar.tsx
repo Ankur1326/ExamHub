f0f3c3ba6a59d6269e3b560687879a87cd35f0e0
Ankur1326 <swamiankur46@gmail.com>
2024-08-31 11:07:12 +0530

    remove un-use loader

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,22 @@ import React from "react";
 import ProfileMenu from "./ProfileMenu";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
-import { Blocks, Loader2 } from "lucide-react";
+import { ClipLoader } from "react-spinners";
 
 function Navbar() {
     const isLoading = useSelector((state: RootState) => state.loading.isLoading);
 
     return (
-        <nav className="text-black flex gap-2 items-center">
-            {
-                isLoading && <Loader2 className="animate-spin" width={20} />
-            }
+        <nav className="text-black flex gap-3 items-center">
+            <div className="relative flex items-center">
+                {/* Other Navbar items can go here */}
+                {isLoading && (
+                    <div className="flex items-center space-x-2">
+                        <ClipLoader color="#3699FF" size={30} />
+                        <span className="text-sm text-blue-500 animate-pulse">Loading...</span>
+                    </div>
+                )}
+            </div>
             <ProfileMenu />
         </nav>
     )
