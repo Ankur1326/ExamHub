6ae15e37dd760516e096faeb74f2d54f15b848ba
Ankur1326 <swamiankur46@gmail.com>
2024-09-09 11:04:03 +0530

    remove everything from dashboard page

--- src/app/(app)/admin/dashboard/page.tsx
+++ src/app/(app)/admin/dashboard/page.tsx
@@ -1,15 +1,4 @@
-'use client'
-import { useState } from 'react';
-// import dynamic from 'next/dynamic';
-
 export default function Dashboard() {
-    const [content, setContent] = useState<string>('');
-    const apiKey = 'rmu0izos7rryo0k697oqhgpm6y96bln99xlzutgii5gausl1';
-
-    const handleEditorChange = (newContent: string) => {
-        setContent(newContent);
-    };
-
     return (
         <div className="w-full h-screen flex items-center justify-center ">
             <p className="text-3xl opacity-35">
