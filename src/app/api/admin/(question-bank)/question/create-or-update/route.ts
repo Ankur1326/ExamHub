c61c933e75a29fd86541a4c5d46156e3aef6ff8f
Ankur1326 <swamiankur46@gmail.com>
2024-09-25 19:36:42 +0530

    develop for step first and second

---
+++ src/app/api/admin/(question-bank)/question/create-or-update/route.ts
@@ -0,0 +1,326 @@
+import dbConnect from "@/lib/dbConnect";
+import Question, { QuestionType } from "@/model/Question";
+import Skill from "@/model/Skill";
+import Tag from "@/model/Tag";
+import Topic from "@/model/Topic";
+import mongoose from "mongoose";
+// import { getSession } from "next-auth/react";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    // const session = await getSession({ request })
+
+    // if (!session) {
+    //     return Response.json(
+    //         {
+    //             success: false,
+    //             message: "Unauthorized. Please log in."
+    //         },
+    //         {
+    //             status: 401
+    //         }
+    //     );
+    // }
+
+    // Check if the user has admin role
+    // if (session.user?.role !== "admin") {
+    //     return res.status(403).json({ error: "Forbidden. You do not have permission to perform this action." });
+    // }
+
+    try {
+        const {
+            step, // which step is being processed
+            questionType,
+            question,
+            options,
+            correctOptions,
+            matchPairs,
+            sequenceOrder,
+            trueFalseAnswer,
+            shortAnswer,
+            fillInTheBlanks,
+            skillName,
+            topicName,
+            tagNames,
+            difficultyLevel,
+            defaultMarks,
+            defaultTimeToSolve,
+            isActive,
+            solution,
+            enableSolutionVideo,
+            solutionVideoType,
+            solutionVideoLink,
+            hint,
+            questionId
+        } = await request.json();
+
+        // console.log(step,
+        //     step,
+        //     questionType,
+        //     question,
+        //     options,
+        //     correctOptions,
+        //     matchPairs,
+        //     sequenceOrder,
+        //     trueFalseAnswer,
+        //     shortAnswer,
+        //     fillInTheBlanks,
+        //     skillName,
+        //     topicName,
+        //     tagNames,
+        //     difficultyLevel,
+        //     defaultMarks,
+        //     defaultTimeToSolve,
+        //     isActive,
+        //     solution,
+        //     enableSolutionVideo,
+        //     solutionVideoType,
+        //     solutionVideoLink,
+        //     hint,
+        //     questionId
+        // );
+
+
+        // Step 1: Create basic question details
+        if (step === 1) {
+            // Validate required fields
+            if (!question) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Question is required",
+                    },
+                    { status: 400 }
+                );
+            }
+
+            // update 
+            let savedQuestion;
+            if (questionId) {
+                savedQuestion = await Question.findByIdAndUpdate(
+                    questionId,
+                    {
+                        questionType,
+                        question,
+                        options,
+                        correctOptions,
+                        matchPairs,
+                        sequenceOrder,
+                        trueFalseAnswer,
+                        shortAnswer,
+                        fillInTheBlanks,
+                    },
+                    { new: true } // Return the updated document
+                );
+                if (!savedQuestion) {
+                    return Response.json(
+                        {
+                            success: false,
+                            message: "Question not found",
+                        },
+                        { status: 404 }
+                    );
+                }
+                return Response.json(
+                    {
+                        success: true,
+                        message: "Step 1 completed. Question updated.",
+                        questionId: savedQuestion._id, // return the updated ID for next step
+                    },
+                    { status: 200 }
+                );
+            } else {
+                // Validate required fields
+                if (!questionType) {
+                    return Response.json(
+                        {
+                            success: false,
+                            message: "Question type is required",
+                        },
+                        { status: 400 }
+                    );
+                }
+                // Check if the questionType is valid
+                if (!Object.values(QuestionType).includes(questionType)) {
+                    return Response.json(
+                        {
+                            success: false,
+                            message: `Invalid question type. Allowed types are: ${Object.values(QuestionType).join(", ")}`,
+                        },
+                        { status: 400 }
+                    );
+                }
+
+                const newQuestion = new Question({
+                    questionType,
+                    question,
+                    options,
+                    correctOptions,
+                    matchPairs,
+                    sequenceOrder,
+                    trueFalseAnswer,
+                    shortAnswer,
+                    fillInTheBlanks,
+                });
+
+                savedQuestion = await newQuestion.save();
+
+                return Response.json(
+                    {
+                        success: true,
+                        message: "Step 1 completed",
+                        questionId: savedQuestion._id, // return the ID for the next step
+                    },
+                    { status: 201 }
+
+                );
+            }
+        }
+
+        // Step 2: Update question with skill, topic, tags, etc.
+        if (step === 2) {
+            console.log(step, questionId, skillName, difficultyLevel, defaultMarks, defaultTimeToSolve, topicName, tagNames);
+
+            if (!questionId || !skillName || !difficultyLevel || !defaultMarks || !defaultTimeToSolve) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Question ID is required",
+                    },
+                    { status: 400 }
+                );
+            }
+
+            // Fetch skill, topic, and tags in parallel
+            const [skill, topic, tags] = await Promise.all([
+                Skill.findOne({ name: skillName }),
+                Topic.findOne({ name: topicName }),
+                Tag.find({ name: { $in: tagNames } }) // Assuming you are searching for multiple tag names
+            ]);
+
+            console.log(skill, topic, tags);
+
+
+            if (!skill || !topic) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Invalid skill or topic"
+                    }, { status: 400 }
+                )
+            }
+
+            // Extract tag IDs
+            const tagIds = tags.map((tag: any) => new mongoose.Types.ObjectId(tag._id));
+
+            console.log(tagIds);
+
+            const updatedQuestion = await Question.findByIdAndUpdate(
+                questionId,
+                {
+                    sectionId: skill.sectionId,
+                    skillId: skill._id,
+                    topicId: topic._id,
+                    tags: tagIds,
+                    difficultyLevel,
+                    defaultMarks,
+                    defaultTimeToSolve
+                },
+                { new: true }
+            );
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Step 2 completed",
+                    updatedQuestion,
+                },
+                { status: 200 }
+            );
+        }
+
+        // Step 3: Update question with solution details
+        if (step === 3) {
+            if (!questionId) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Question ID is required for Step 3",
+                    },
+                    { status: 400 }
+                );
+            }
+
+            const updatedQuestion = await Question.findByIdAndUpdate(
+                questionId,
+                {
+                    solution,
+                    enableSolutionVideo,
+                    solutionVideoType,
+                    solutionVideoLink,
+                    hint,
+                },
+                { new: true }
+            );
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Step 3 completed",
+                    updatedQuestion,
+                },
+                { status: 200 }
+            );
+        }
+
+        // Step 4: Finalize question
+        if (step === 4) {
+            if (!questionId) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Question ID is required for Step 4",
+                    },
+                    { status: 400 }
+                );
+            }
+
+            const updatedQuestion = await Question.findByIdAndUpdate(
+                questionId,
+                { isActive },
+                { new: true }
+            );
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Step 4 completed. Question is now finalized",
+                    updatedQuestion,
+                },
+                { status: 200 }
+            );
+        }
+
+        return Response.json(
+            {
+                success: false,
+                message: "Invalid step or missing required data.",
+            },
+            { status: 400 }
+        );
+
+    } catch (error) {
+        console.log("Error while creating new question : ", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Internal server error while creating question",
+                error
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}
