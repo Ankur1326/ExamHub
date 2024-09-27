b383eb1f887db5e01f197aac895233bf7f4b2c01
Ankur1326 <swamiankur46@gmail.com>
2024-09-27 14:10:53 +0530

    create CustomSKEditor

--- src/components/CustomCKEditor.tsx
+++ src/components/CustomCKEditor.tsx
@@ -1,7 +1,9 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Math from '@isaul32/ckeditor5-math/src/math';
+import AutoformatMath from '@isaul32/ckeditor5-math/src/autoformatmath';
 
 interface CustomCKEditorProps {
     content: string;
@@ -14,10 +16,48 @@ const CKEditor = dynamic<any>(() =>
 );
 
 const CustomCKEditor: React.FC<CustomCKEditorProps> = ({ content, setContent }) => {
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+        // Load MathJax
+        const script = document.createElement('script');
+        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML';
+        script.async = true;
+        document.body.appendChild(script);
+
+        script.onload = () => {
+            // Configure MathJax
+            MathJax.Hub.Config({
+                tex2jax: {
+                    inlineMath: [['$', '$'], ['\\(', '\\)']],
+                    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+                }
+            });
+        };
+    }, []);
+
+    const editorConfiguration = {
+        plugins: [Math, AutoformatMath],
+        toolbar: [
+            'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList',
+            'blockQuote', 'insertTable', 'undo', 'redo', '|', 'math'
+        ],
+        math: {
+            engine: 'mathjax', // Use MathJax engine
+            outputType: 'script', // MathJax output type
+            forceOutputType: false, // Allow both script and span types
+            enablePreview: true, 
+        },
+    };
+
     return (
         <CKEditor
             editor={ClassicEditor}
             data={content}
+            config={editorConfiguration}
+            onReady={(editor:any) => {
+                editorRef.current = editor;
+            }}
             onChange={(event: any, editor: any) => setContent(editor.getData())}
         />
     );
