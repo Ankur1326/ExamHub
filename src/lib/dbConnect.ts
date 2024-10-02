c66fcfd68f16b8827f479a9c2974ab131cd674ff
Ankur1326 <swamiankur46@gmail.com>
2024-10-02 15:31:22 +0530

    fixed conection string name

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -11,9 +11,8 @@ async function dbConnect(): Promise<void> {
         console.log("Already connected to database");
         return
     }
-
     try {
-        const db = await mongoose.connect(`${process.env.MONGOexamHubDB_URI}/examHub` as string)
+        const db = await mongoose.connect(`${process.env.MONGODB_URI}/examHub` as string)
 
         connection.isConnected = db.connections[0].readyState
         console.log("db connected successfully");
